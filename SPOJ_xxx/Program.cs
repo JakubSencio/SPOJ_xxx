ceb4a5ff082f5f392d824b3a518d6347d4d53fa2
JacobKomp <sencio.jakub@Gmail.com>
2023-04-18 21:23:54 +0200

    522 Done

--- SPOJ_xxx/Program.cs
+++ SPOJ_xxx/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            // Main BRANCH
+            {
+                int numberOfSum = int.Parse(Console.ReadLine());
+                int sum = 0;
+                for (int i = 0; i < numberOfSum; i++)
+                {
+                    string[] line = Console.ReadLine().Split();
+                    int a = int.Parse(line[0]);
+                    int b = int.Parse(line[1]);
+                    sum = a + b;
+                    Console.WriteLine(sum);
+                }
+
+
+            }
         }
+    }
+}
     }
 }
