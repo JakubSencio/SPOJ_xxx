cf40634b7a2c2634cc2a88f5061248b3ceb53f47
JacobKomp <sencio.jakub@Gmail.com>
2023-04-18 21:22:23 +0200

    SPOJ_506 Done.

--- SPOJ_xxx/Program.cs
+++ SPOJ_xxx/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            // Main BRANCH
+            string inputWord = Console.ReadLine().ToUpper();
+            int wordLength = inputWord.Length;
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                char currentLetter = inputWord[i];
+                int repetitions = 1;
+
+                for (int j = i + 1; j < wordLength; j++)
+                {
+                    if (inputWord[j] == currentLetter)
+                    {
+                        repetitions++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (repetitions >= 3)
+                {
+                    Console.Write($"{repetitions}{currentLetter}");
+                    i += repetitions - 1;
+                }
+                else
+                {
+                    Console.Write($"{currentLetter}");
+                }
+            }
         }
+
+
     }
+}
 }
