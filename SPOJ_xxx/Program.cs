82fa022a8fe19b160dafc7439fe7ab87ba84dd89
JacobKomp <sencio.jakub@Gmail.com>
2023-04-23 16:15:50 +0200

    617 done

--- SPOJ_xxx/Program.cs
+++ SPOJ_xxx/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace SPOJ_xxx
+﻿using System.Collections.Concurrent;
+using System.Security.Cryptography;
+
+namespace SPOJ_xxx
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Main BRANCH
+
+            string input = Console.ReadLine();
+            string[] parts = input.Split(' ');
+            string firstWord = parts[0];
+            string secondWord = parts[1];
+
+            int lengthFirst = firstWord.Length;
+
+            int lengthSecond = secondWord.Length;
+
+
+            if(lengthFirst > lengthSecond || lengthFirst == lengthSecond)
+            {
+                for (int i = 0; i < lengthSecond; i++)
+                {
+                    Console.Write(firstWord[i]);
+                    Console.Write(secondWord[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < lengthFirst; i++)
+                {
+                    Console.Write(firstWord[i]);
+                    Console.Write(secondWord[i]);
+                }
+            }
+
         }
     }
 }
